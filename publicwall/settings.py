144066f72058ec1e3506b0ba9cb4e83694879cb6
info26 <38963122+info26@users.noreply.github.com>
2020-04-16 15:15:41 +0000

    add env checking for debug

--- publicwall/settings.py
+++ publicwall/settings.py
@@ -23,7 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv("debug") == "true":
+
+  DEBUG = True
+else:
+  DEBUG - False
 
 ALLOWED_HOSTS = ['https://django-public-wall.herokuapp.com/']
 
